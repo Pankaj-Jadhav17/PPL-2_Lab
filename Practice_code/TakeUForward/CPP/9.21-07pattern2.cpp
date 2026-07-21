615b073582575262db9c35f12968157910279e70
Pankaj-Jadhav17 <pankajjadhav1712@gmail.com>
2026-07-21 10:01:50 +0530

    print left to right star pattern

---
+++ Practice_code/TakeUForward/CPP/9.21-07pattern2.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class solution{
+    public:
+      int Pattern2(int n){
+          for(int i=0;i<n;i++){
+              for(int j=0;j<=i;j++){
+                  //if(i==0 || i==n-1 || j==0 || j==n-1){
+                      cout<<"*";
+                  //}
+                  //else{
+                      cout<<" ";
+                  //}
+              }
+              cout<<endl;
+          }
+          return 0;
+      }
+};
+
+int main(){
+    solution obj;
+    int n;
+    cout<<"Enter the number of rows and columns: ";
+    cin>>n;
+    obj.Pattern2(n);
+    return 0;
+}
